30fa6482fab4bbce6c7585c1b0e6a752c486832f
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-04 17:42:32 -0700

    Slowly starting to set up the vector

--- colorStruct.cpp
+++ colorStruct.cpp
@@ -1,6 +1,9 @@
 //COMSC-210 | Lab 4 | Eric-Giulio Hedes
 #include <iostream>
+#include <iomanip>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 //Declare the color struct
@@ -12,10 +15,15 @@ struct Color {
 //Start of main()
 int main()
 {
+    srand(time(0));
+
     Color test1;
-    test1.red = 128;
-    test1.green = 90;
-    test1.blue = 100;
-    cout << test1.red << " " << test1.blue << " " << test1.green << endl;
+    test1.red = rand() % 255;
+    test1.blue = rand() % 255;
+    test1.green = rand() % 255;
+    vector<Color> clist;
+    clist.push_back(test1);
+
+    cout << clist[0].red << setw(10) << clist[0].blue << setw(10) << clist[0].green << endl;
 }
 //End of main()
