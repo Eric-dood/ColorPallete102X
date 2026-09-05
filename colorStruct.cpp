d7d6dac97dc8f0d126bd990044a9438f0b906ab1
Eric-Giulio Hedes <literally-bugsie@Eric-Giulios-MacBook-Air.local>
2026-09-04 17:57:46 -0700

    Setting up the code for Milestone 3

--- colorStruct.cpp
+++ colorStruct.cpp
@@ -24,6 +24,17 @@ int main()
     vector<Color> clist;
     clist.push_back(test1);
 
+    //Initialize n, which is a random number between 25 and 50.
+    int n = rand() % 25 + 25;
+    for (int i = n; i > 0; i--)
+    {
+        Color t;
+        t.red = rand() % 255;
+        t.blue = rand() % 255;
+        t.green = rand() % 255;
+        clist.push_back(t);
+    }
+
     cout << clist[0].red << setw(10) << clist[0].blue << setw(10) << clist[0].green << endl;
 }
 //End of main()
